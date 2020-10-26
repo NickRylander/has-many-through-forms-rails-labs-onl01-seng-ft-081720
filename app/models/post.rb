76081f94f3089c35529f91957c5a331dcba30e0d
NickRylander <nick.rylander@outlook.com>
2020-10-26 15:27:09 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,15 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-
+  accepts_nested_attributes_for :categories
+  
+  def categories_attributes=(attrs)
+    attrs.values.each do |c_attr|
+      if c_attr[:name] != ""
+        category = Category.find_or_create_by(c_attr)
+        self.categories << category
+      end
+    end
+  end
 
 end
